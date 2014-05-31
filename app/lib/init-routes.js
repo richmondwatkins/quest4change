f1c2bb65441b20545984d4870a4a0495ecd833b2
Jason Pullias <jason@pullias.com>
2014-05-31 17:19:35 -0500

    add code to pull locations from socrata API and put in db

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -16,9 +16,14 @@ module.exports = (req, res, next)=>{
 function load(app, fn){
   var home = traceur.require(__dirname + '/../routes/home.js');
   var users = traceur.require(__dirname + '/../routes/users.js');
+  var locations = traceur.require(__dirname + '/../routes/locations.js');
 
   app.get('/', dbg, home.index);
   app.get('/help', dbg, home.help);
+  // routes to pull data from socrata portal and add location objects to DB
+  app.post('/addArt', dbg, locations.addArt);
+  app.post('/addHistory', dbg, locations.addHistory);
+  app.post('/addPark', dbg, locations.addPark);
 
   app.post('/register', dbg, users.register);
   console.log('Routes Loaded');
