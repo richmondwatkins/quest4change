030c19f3c3248274b8049d2ee94cc4ce8cebb6ff
Aaron Johnson <ajohnson13.aj@gmail.com>
2014-06-01 13:42:53 -0500

    groups styling

--- app/static/js/es6/groups.es6.js
+++ app/static/js/es6/groups.es6.js
@@ -4,19 +4,31 @@
 
   function init(){
     $('#new-group').click(makeGroup);
+    $('.group-cat').hide();
+    $('#show-createdgroup-cat').click(showCreatedGroups);
+    $('#show-mygroup-cat').click(showMyGroups);
+    $('#show-allgroup-cat').click(showAllGroups);
   }
 
+  function showAllGroups(){
+    $('#all-groups').slideToggle();    
+  }
 
+  function showMyGroups(){
+    $('#mygroups').slideToggle();
+  }
+
+  function showCreatedGroups(){
+    $('#createdgroups').slideToggle();
+  }
 
   function makeGroup(){
 
     ajax('/groups/new', 'get', null, html=>{
-      $('#all-groups').append(html);
+      $('#new-group-form').empty().append(html);
     });
 
   }
-
-
 
 
   function ajax(url, type, data={}, success=r=>console.log(r), dataType='html'){
