2293d21f9d239e205615a5f72b99f52a6856be1f
Nathan Hood <nathanhood@me.com>
2014-06-01 00:07:20 -0500

    group edit

--- app/models/group.js
+++ app/models/group.js
@@ -1,16 +1,29 @@
 'use strict';
 
-// var quests = global.nss.db.collection('quests');
-//
+var groups = global.nss.db.collection('groups');
+var _ = require('lodash');
 //
 class Group{
   constructor(ownerId, groupName, fn){
     this.name = groupName;
+    this.groupNumber = groupNumber();
     this.owner = ownerId;
     this.members = [];
     this.isLocked = false;
     this.quests = [];
   }
+
+}
+
+function groupNumber(){
+  var num = _.random(0,5);
+  groups.findOne({groupNumber:num}, (err, number)=>{
+    if(number){
+      groupNumber();
+    }else{
+      return num;
+    }
+  });
 }
 //
 //
